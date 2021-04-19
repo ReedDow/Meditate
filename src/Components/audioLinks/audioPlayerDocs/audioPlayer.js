2a4ce97205fc4dced9f06f5f7b24e8ee4cc07eb8
ReedDow <reed.r.dow@gmail.com>
2021-04-19 17:07:30 -0600

    meditation audio

--- src/Components/audioLinks/audioPlayerDocs/audioPlayer.js
+++ src/Components/audioLinks/audioPlayerDocs/audioPlayer.js
@@ -1,5 +1,5 @@
 import { useState, useRef } from 'react'
-import song from './Suncrown - Legend of the Forgotten Centuries.mp3'
+import song from './Deep breath meditation.m4a'
 import Slider from './slider/Slider'
 import ControlPanel from './controls/ControlPanel'
 import './audioStyle.css'
